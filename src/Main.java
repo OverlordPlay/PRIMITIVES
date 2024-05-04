c0949057cba526938efd0fe14a88a8d829209fa3
Learn <egivmir59@gmail.com>
2024-05-04 11:00:04 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5_411;
+
+        int bonus = amount / 20;
+
+        System.out.println("Итоговый Бонус: " + bonus);
+    }
+}
